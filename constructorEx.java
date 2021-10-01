42ad13bd437ca14d16c598ecee4a09dbba707c73
Kiran <girs19cs@cmrit.ac.in>
2021-10-01 16:13:42 +0530

    one

--- constructorEx.java
+++ constructorEx.java
@@ -5,7 +5,7 @@ class text{
          System.out.println("int-arg constructor");
         }
         text(){
-          System.out.println("no arg constructor");
+          System.out.println("no-arg constructor");
         }
     public static void main(String[] A){
         text a=new text(10);
